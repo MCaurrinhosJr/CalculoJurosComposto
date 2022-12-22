e6678e3c3265eff0469e529c1c4969517d0d098f
Marcelo <mclcaurrinhos@gmail.com>
2022-12-22 17:52:37 -0300

    .

--- CalculoJurosComposto/Controllers/CalculaJurosController.cs
+++ CalculoJurosComposto/Controllers/CalculaJurosController.cs
@@ -8,13 +8,23 @@ namespace CalculoJurosComposto.Controllers
     [ApiController]
     public class CalculaJurosController : ControllerBase
     {
-        // GET CalculaJuros?valorinicial=value&meses=value2
+        
         [HttpGet]
         public async Task<ActionResult> CalculaJurosAsync(double valorinicial, int meses)
         {
             double juros = await ApiHelper.GetAsync("Taxa");
                         
             double result = valorinicial * Math.Pow((1 + juros), meses);
+
+            return Ok(result.ToString("N2"));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CalculaJurosAsync(BaseCalculo valores)
+        {
+            double juros = await ApiHelper.GetAsync("Taxa");
+
+            double result = valores.ValorInicial * Math.Pow((1 + juros), valores.Meses);
 
             return Ok(result.ToString("N2"));
         }
